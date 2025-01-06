ada43c184e8cc5d29e81c6f3f9f1a39b489c234d
govno-scripts <tr34432065@gmail.com>
2025-01-06 18:34:25 +0300

    Update PlayerTeleport.cs

--- PlayerTeleport.cs
+++ PlayerTeleport.cs
@@ -1,79 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;  // Для работы с UI
 
-public class PlayerTeleport : MonoBehaviour
+
+public class NPC : MonoBehaviour
 {
-    // Точка возрождения
-    private Transform respawnPoint;
-
-    // UI элементы
-    public Text deathMessageText;  // Для отображения сообщения "Ты умер"
-    private bool isDead = false;
-
-    private void Start()
+    public int health = 5;
+    public int level = 1;
+    public float speed = 1.0f;
+    public float rotation = 0.0f;
+    // Start is called before the first frame update
+    void Start()
     {
-        // Поиск блока с тегом 'Respawn'
-        GameObject respawnBlock = GameObject.FindGameObjectWithTag("Respawn");
-        if (respawnBlock != null)
-        {
-            respawnPoint = respawnBlock.transform;
-        }
-        else
-        {
-            Debug.LogError("Точка возрождения (Respawn) не найдена! Добавьте объект с тегом 'Respawn'.");
-        }
-
-        // Прячем сообщение о смерти в начале
-        if (deathMessageText != null)
-        {
-            deathMessageText.gameObject.SetActive(false);
-        }
+        
     }
 
-    private void OnCollisionEnter(Collision collision)
+    // Update is called once per frame
+    void Update()
     {
-        // Проверка столкновения с объектом, у которого тег 'Killer'
-        if (collision.gameObject.CompareTag("Killer"))
-        {
-            if (!isDead)
-            {
-                isDead = true;
-                ShowDeathMessage();
-                Invoke("Teleport", 0.01f);  // Телепортируем через 1 секунду
-            }
-        }
-    }
+        Vector3 pose = transform.position;
+        pose.z += speed * Time.deltaTime;
+        transform.position = pose;
 
-    private void ShowDeathMessage()
-    {
-        if (deathMessageText != null)
-        {
-            deathMessageText.text = "Ты умер!";
-            deathMessageText.color = Color.red;  // Красный цвет текста
-            deathMessageText.gameObject.SetActive(true);  // Показываем сообщение
-        }
-    }
-
-    private void Teleport()
-    {
-        if (respawnPoint != null)
-        {
-            transform.position = respawnPoint.position;
-            transform.rotation = respawnPoint.rotation;
-            Debug.Log($"{gameObject.name} был телепортирован на точку Respawn!");
-        }
-        else
-        {
-            Debug.LogError("Ошибка: Точка Respawn не установлена.");
-        }
-
-        // Скрыть сообщение после телепортации
-        if (deathMessageText != null)
-        {
-            deathMessageText.gameObject.SetActive(false);
-        }
-
-        // Сброс флага смерти
-        isDead = false;
     }
 }
